cca9f261d19ba3e509d6a0e6a8e12ca3952255eb
Thomas Whiteway <thomas.whiteway@gmail.com>
2023-12-02 15:40:45 +0000

    Day 2 part 1

---
+++ src/day02.rs
@@ -0,0 +1,104 @@
+use failure::Error;
+use nom::complete;
+
+mod parse {
+    use failure::{err_msg, Error};
+    use nom::{
+        branch::alt,
+        bytes::complete::tag,
+        character::complete::newline,
+        combinator::{all_consuming, map, value},
+        multi::{many1, separated_list1},
+        sequence::{preceded, separated_pair, terminated, tuple},
+        IResult,
+    };
+
+    use crate::parsers::unsigned;
+
+    use super::Colour;
+
+    fn colour(input: &str) -> IResult<&str, Colour> {
+        alt((
+            value(Colour::Blue, tag("blue")),
+            value(Colour::Red, tag("red")),
+            value(Colour::Green, tag("green")),
+        ))(input)
+    }
+
+    fn amount(input: &str) -> IResult<&str, (usize, Colour)> {
+        separated_pair(unsigned, tag(" "), colour)(input)
+    }
+
+    fn round(input: &str) -> IResult<&str, [usize; 3]> {
+        map(separated_list1(tag(", "), amount), |amounts| {
+            let mut result = [0; 3];
+            for (num, colour) in amounts {
+                result[colour as usize] += num;
+            }
+            result
+        })(input)
+    }
+
+    fn game(input: &str) -> IResult<&str, Vec<[usize; 3]>> {
+        preceded(
+            tuple((tag("Game "), unsigned::<usize>, tag(": "))),
+            separated_list1(tag("; "), round),
+        )(input)
+    }
+
+    fn games(input: &str) -> IResult<&str, Vec<Vec<[usize; 3]>>> {
+        many1(terminated(game, newline))(input)
+    }
+
+    pub fn parse_input(input: &str) -> Result<Vec<Vec<[usize; 3]>>, Error> {
+        all_consuming(games)(input)
+            .map(|(_, games)| games)
+            .map_err(|err| err_msg(format!("Failed to parse input: {}", err)))
+    }
+}
+
+use parse::parse_input;
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+enum Colour {
+    Red,
+    Green,
+    Blue,
+}
+
+fn is_round_possible(round: &[usize; 3], candidate: &[usize; 3]) -> bool {
+    round.iter().zip(candidate.iter()).all(|(x, y)| x <= y)
+}
+
+fn is_game_possible(rounds: &[[usize; 3]], candidate: &[usize; 3]) -> bool {
+    rounds
+        .iter()
+        .all(|round| is_round_possible(round, candidate))
+}
+
+pub struct Solver {}
+
+impl super::Solver for Solver {
+    type Problem = Vec<Vec<[usize; 3]>>;
+
+    fn parse_input(data: String) -> Result<Self::Problem, Error> {
+        parse_input(&data)
+    }
+
+    fn solve(games: Self::Problem) -> (Option<String>, Option<String>) {
+        let candidate = [12, 13, 14];
+
+        let part1: usize = (1..)
+            .zip(games)
+            .filter_map(|(game_id, game)| {
+                if is_game_possible(&game, &candidate) {
+                    Some(game_id)
+                } else {
+                    None
+                }
+            })
+            .sum();
+
+        (Some(part1.to_string()), None)
+    }
+}
