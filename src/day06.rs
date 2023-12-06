acd2a0957bf0db3cdb684d8d66bf8cb4b3a69d90
Thomas Whiteway <thomas.whiteway@gmail.com>
2023-12-06 08:50:56 +0000

    Day 6 part 2

--- src/day06.rs
+++ src/day06.rs
@@ -2,7 +2,7 @@ use crate::parsers::unsigned;
 use failure::{err_msg, Error};
 use nom::{
     bytes::complete::tag,
-    character::complete::{newline, space1},
+    character::complete::{digit1, newline, space1},
     combinator::{all_consuming, map},
     multi::separated_list1,
     sequence::delimited,
@@ -55,38 +55,68 @@ impl Race {
     }
 }
 
-fn number_list<'a, 'b>(name: &'a str) -> impl FnMut(&'b str) -> IResult<&'b str, Vec<u64>> + 'a
+fn named_value<'a, 'b, F, A>(
+    name: &'a str,
+    value_parser: F,
+) -> impl FnMut(&'b str) -> IResult<&'b str, A> + 'a
 where
+    F: FnMut(&str) -> IResult<&str, A> + 'static,
+    A: 'static,
     'b: 'a,
 {
-    delimited(tuple((tag(name), tag(":"), space1)), numbers, newline)
+    delimited(tuple((tag(name), tag(":"), space1)), value_parser, newline)
 }
 
 fn numbers(input: &str) -> IResult<&str, Vec<u64>> {
     separated_list1(space1, unsigned)(input)
 }
 
+fn parse_small_races(data: &str) -> Result<Vec<Race>, Error> {
+    let times = named_value("Time", numbers);
+    let distances = named_value("Distance", numbers);
+    let races = map(tuple((times, distances)), |(ts, ds)| {
+        ts.into_iter()
+            .zip(ds)
+            .map(|(time, distance)| Race { time, distance })
+            .collect()
+    });
+    all_consuming(races)(data)
+        .map(|(_, races)| races)
+        .map_err(|err| err_msg(format!("Failed to parse input: {}", err)))
+}
+
+fn distributed_number(input: &str) -> IResult<&str, u64> {
+    map(separated_list1(space1, digit1), |segments| {
+        segments.join("").parse().unwrap()
+    })(input)
+}
+
+fn parse_big_race(data: &str) -> Result<Race, Error> {
+    let time = named_value("Time", distributed_number);
+    let distance = named_value("Distance", distributed_number);
+    let race = map(tuple((time, distance)), |(time, distance)| Race {
+        time,
+        distance,
+    });
+    all_consuming(race)(data)
+        .map(|(_, race)| race)
+        .map_err(|err| err_msg(format!("Failed to parse input: {}", err)))
+}
+
 pub struct Solver {}
 
 impl super::Solver for Solver {
-    type Problem = Vec<Race>;
+    type Problem = (Vec<Race>, Race);
 
     fn parse_input(data: String) -> Result<Self::Problem, Error> {
-        let times = number_list("Time");
-        let distances = number_list("Distance");
-        let races = map(tuple((times, distances)), |(ts, ds)| {
-            ts.into_iter()
-                .zip(ds)
-                .map(|(time, distance)| Race { time, distance })
-                .collect()
-        });
-        all_consuming(races)(&data)
-            .map(|(_, races)| races)
-            .map_err(|err| err_msg(format!("Failed to parse input: {}", err)))
+        let small_races = parse_small_races(&data)?;
+        let big_race = parse_big_race(&data)?;
+        Ok((small_races, big_race))
     }
 
-    fn solve(races: Self::Problem) -> (Option<String>, Option<String>) {
-        let part1: u64 = races.iter().map(|race| race.ways_to_win()).product();
-        (Some(part1.to_string()), None)
+    fn solve((small_races, big_race): Self::Problem) -> (Option<String>, Option<String>) {
+        let part1: u64 = small_races.iter().map(|race| race.ways_to_win()).product();
+        let part2: u64 = big_race.ways_to_win();
+        (Some(part1.to_string()), Some(part2.to_string()))
     }
 }
