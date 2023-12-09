e0ea6631ed406d4308476b7970f27ef552dc43d8
Thomas Whiteway <thomas.whiteway@gmail.com>
2023-12-09 11:11:52 +0000

    Day 9 part 2

--- src/day09.rs
+++ src/day09.rs
@@ -1,8 +1,8 @@
 use failure::{err_msg, Error};
 use itertools::Itertools;
 
-fn find_next_value(values: &[i64]) -> i64 {
-    let mut stack: Vec<Vec<i64>> = vec![values.iter().cloned().collect()];
+fn find_prev_next_value(values: &[i64]) -> (i64, i64) {
+    let mut stack: Vec<Vec<i64>> = vec![values.to_vec()];
     while !stack.last().unwrap().iter().all(|val| *val == 0) {
         stack.push(
             stack
@@ -16,13 +16,16 @@ fn find_next_value(values: &[i64]) -> i64 {
     }
 
     stack.last_mut().unwrap().push(0);
+    stack.last_mut().unwrap().insert(0, 0);
 
     for index in (0..stack.len() - 1).rev() {
-        let new_val = stack[index].last().unwrap() + stack[index + 1].last().unwrap();
-        stack[index].push(new_val);
+        let start_val = stack[index].first().unwrap() - stack[index + 1].first().unwrap();
+        stack[index].insert(0, start_val);
+        let end_val = stack[index].last().unwrap() + stack[index + 1].last().unwrap();
+        stack[index].push(end_val);
     }
 
-    *stack[0].last().unwrap()
+    (*stack[0].first().unwrap(), *stack[0].last().unwrap())
 }
 
 pub struct Solver {}
@@ -44,7 +47,10 @@ impl super::Solver for Solver {
     }
 
     fn solve(series: Self::Problem) -> (Option<String>, Option<String>) {
-        let part1: i64 = series.iter().map(|values| find_next_value(values)).sum();
-        (Some(part1.to_string()), None)
+        let (part2, part1) = series
+            .iter()
+            .map(|values| find_prev_next_value(values))
+            .fold((0, 0), |(tot_x, tot_y), (x, y)| (tot_x + x, tot_y + y));
+        (Some(part1.to_string()), Some(part2.to_string()))
     }
 }
